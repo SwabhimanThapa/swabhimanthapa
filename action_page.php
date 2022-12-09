dda38193841e4a61fae79869728dd2c026cfea06
SwabhimanThapa <109540899+SwabhimanThapa@users.noreply.github.com>
2022-12-09 18:18:18 +0545

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<script>
+if (user="Swabhiman" )
+{
+           if(pwd="@Swath58myw" )
+           {
+            <a href="personaldetail.html" >
+           }
+}
+else
+ {
+<a href="login.html">
+} 
+</script>
+
+</body>
+</html>
